c46325f8eafb156c760a4ca19d315212c1787cfd
Arthur Silva <arthur.gbs.dev@gmail.com>
2026-08-04 23:14:04 -0300

    Criando o Global Handler Exception, e cobrindo todas as 4 exceptions personalizadas do projeto, com os codigos http corretos de retorno

---
+++ src/main/java/io/github/arthursilvagbs/Locacao/de/Carros/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package io.github.arthursilvagbs.Locacao.de.Carros.exceptions;
+
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+   @ExceptionHandler(EntidadeNaoEncontradaException.class)
+   public ResponseEntity<String> handlerEntidadeNaoEncontrada(EntidadeNaoEncontradaException e) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+   }
+
+   @ExceptionHandler(RegistroDuplicadoException.class)
+   public ResponseEntity<String> handlerRegistroDuplicado(RegistroDuplicadoException e) {
+      return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+   }
+
+   @ExceptionHandler(StatusInvalidoException.class)
+   public ResponseEntity<String> handlerStatusInvalido(StatusInvalidoException e) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+   }
+
+   @ExceptionHandler(VeiculoNaoDisponivelException.class)
+   public ResponseEntity<String> handlerVeiculoNaoDisponivel(VeiculoNaoDisponivelException e) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+   }
+
+}
