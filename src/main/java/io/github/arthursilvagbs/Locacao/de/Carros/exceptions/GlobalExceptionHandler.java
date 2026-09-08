0b49b7fcd9542973d44771075387d113b5cb5b2e
Arthur Silva <arthur.gbs.dev@gmail.com>
2026-09-08 20:06:23 -0300

    Salvando

--- src/main/java/io/github/arthursilvagbs/Locacao/de/Carros/exceptions/GlobalExceptionHandler.java
+++ src/main/java/io/github/arthursilvagbs/Locacao/de/Carros/exceptions/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package io.github.arthursilvagbs.Locacao.de.Carros.exceptions;
 
-import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
